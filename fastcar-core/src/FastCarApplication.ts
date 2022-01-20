beea494b615610228cce01295d61f036cd3cc0c9
zhongjx <1433589043@qq.com>
2022-01-20 16:47:16 +0800

    修正settings配置错误问题

--- fastcar-core/src/FastCarApplication.ts
+++ fastcar-core/src/FastCarApplication.ts
@@ -61,6 +61,11 @@ class FastCarApplication extends Events {
 				Reflect.deleteProperty(fileContent, property);
 				if (CommonConstant.Settings == property) {
 					Object.keys(addConfig).forEach(key => {
+						let beforeConfig = this.sysConfig.settings.get(key);
+						if (beforeConfig) {
+							//对settings的属性进行覆盖
+							Object.assign(beforeConfig, addConfig[key]);
+						}
 						this.sysConfig.settings.set(key, addConfig[key]);
 					});
 				} else {
