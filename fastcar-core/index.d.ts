224389303c4b71141299d6cf01d770fcb27e89dc
zhongjx <1433589043@qq.com>
2022-02-20 15:25:07 +0800

    完善声明

--- fastcar-core/index.d.ts
+++ fastcar-core/index.d.ts
@@ -7,6 +7,7 @@ declare type FIELDTYPE = {
 export enum LifeCycleModule {
 	ApplicationStart = "applicationStart", //应用初始化完全完成后运行
 	ApplicationStop = "applicationStop", //应用结束之前进行执行
+	LoadConfigure = "loadConfigure", //应用加载配置文件
 }
 
 export enum ComponentKind {
