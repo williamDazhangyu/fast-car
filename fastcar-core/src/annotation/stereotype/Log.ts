ac4315898b16f14848ee96c043b7b1d217efe5e5
zhongjx <1433589043@qq.com>
2022-03-21 14:53:46 +0800

    修正错误

--- fastcar-core/src/annotation/stereotype/Log.ts
+++ fastcar-core/src/annotation/stereotype/Log.ts
@@ -1,14 +1,14 @@
 import { FastCarMetaData } from "../../constant/FastCarMetaData";
-import Logger from "../../interface/Logger";
+// import Logger from "../../interface/Logger";
 
 //日志实例
 export default function Log(category?: string) {
 	return function(target: any, propertyKey: string) {
-		const designType = Reflect.getMetadata(FastCarMetaData.designType, target, propertyKey);
-		if (designType != Logger) {
-			console.error(`${propertyKey} does not belong to Logger type`);
-			return;
-		}
+		// const designType = Reflect.getMetadata(FastCarMetaData.designType, target, propertyKey);
+		// if (designType != Logger) {
+		// 	console.error(`${propertyKey} does not belong to Logger type`);
+		// 	return;
+		// }
 
 		let m = category || propertyKey;
 		if (Reflect.hasMetadata(FastCarMetaData.LoggerModule, target)) {
--- fastcar-core/target/annotation/stereotype/Log.js
+++ fastcar-core/target/annotation/stereotype/Log.js
@@ -1,15 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const FastCarMetaData_1 = require("../../constant/FastCarMetaData");
-const Logger_1 = require("../../interface/Logger");
+// import Logger from "../../interface/Logger";
 //日志实例
 function Log(category) {
     return function (target, propertyKey) {
-        const designType = Reflect.getMetadata(FastCarMetaData_1.FastCarMetaData.designType, target, propertyKey);
-        if (designType != Logger_1.default) {
-            console.error(`${propertyKey} does not belong to Logger type`);
-            return;
-        }
+        // const designType = Reflect.getMetadata(FastCarMetaData.designType, target, propertyKey);
+        // if (designType != Logger) {
+        // 	console.error(`${propertyKey} does not belong to Logger type`);
+        // 	return;
+        // }
         let m = category || propertyKey;
         if (Reflect.hasMetadata(FastCarMetaData_1.FastCarMetaData.LoggerModule, target)) {
             let loggerMap = Reflect.getMetadata(FastCarMetaData_1.FastCarMetaData.LoggerModule, target);
