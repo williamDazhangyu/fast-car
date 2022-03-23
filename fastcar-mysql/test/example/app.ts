5d2352eeb56ee77038b3e464a4e1b5276e5eb458
zhongjx <1433589043@qq.com>
2022-03-23 18:18:09 +0800

    新增测试用例

--- fastcar-mysql/test/example/app.ts
+++ fastcar-mysql/test/example/app.ts
@@ -1,56 +1,66 @@
 import { FastCarApplication } from "fastcar-core";
-import { Application, ComponentScan, Log } from "fastcar-core/annotation";
+import { Application, BaseFilePath, BasePath } from "fastcar-core/annotation";
 import EnableMysql from "../../src/annotation/EnableMysql";
 import SimpleService from "./service/SimpleService";
 import TestDS from "./service/TestDS";
 import TestTransactional from "./service/TestTransactional";
 
 @Application
+@BasePath(__dirname) //直接运行ts文件时可不用
+@BaseFilePath(__filename)
 @EnableMysql //开启mysql数据库
 class APP {
 	app!: FastCarApplication;
 }
-
 const appInstance = new APP();
 
-console.log("crud测试");
-let service: SimpleService = appInstance.app.getComponentByTarget(SimpleService);
-// 详情看更多的内部测试;
-// service.saveOne().then((res) => {
-// 	console.log(res);
-// });
+describe("mysql测试", () => {
+	it("crud测试", async () => {
+		let service: SimpleService = appInstance.app.getComponentByTarget(SimpleService);
+		// 详情看更多的内部测试;
+		service.saveOne().then(res => {
+			console.log(res);
+		});
 
-// service.selectOne().then((res) => {
-// 	console.log(res);
-// });
-
-let service2: TestTransactional = appInstance.app.getComponentByTarget(TestTransactional);
-// console.log("纯sql测试");
-// service2.exec();
-
-// console.log("事务执行测试");
-// service2
-// 	.work()
-// 	.then((res) => {
-// 		console.log(res);
-// 	})
-// 	.catch((e) => {
-// 		console.error("service2");
-// 		console.error(e);
-// 	});
-
-console.log("事务嵌套测试");
-service2
-	.firstWork()
-	.then(res => {
-		console.log(res);
-	})
-	.catch(e => {
-		console.error(e);
+		service.selectOne().then(res => {
+			console.log(res);
+		});
 	});
 
-// console.log("多数据源测试");
-// let testTestDSService: TestDS = appInstance.app.getComponentByTarget(TestDS);
-// testTestDSService.switchDS().then((res) => {
-// 	console.log(res[0]?.caseName, res[1]?.caseName);
-// });
+	it("事务执行测试", async () => {
+		let service: TestTransactional = appInstance.app.getComponentByTarget(TestTransactional);
+		service.exec();
+
+		console.log("");
+		service
+			.work()
+			.then(res => {
+				console.log(res);
+			})
+			.catch(e => {
+				console.error("service2");
+				console.error(e);
+			});
+	});
+
+	it("事务嵌套测试", async () => {
+		let service: TestTransactional = appInstance.app.getComponentByTarget(TestTransactional);
+
+		service
+			.firstWork()
+			.then(res => {
+				console.log(res);
+			})
+			.catch(e => {
+				console.error(e);
+			});
+	});
+
+	it("多数据源测试", () => {
+		let testTestDSService: TestDS = appInstance.app.getComponentByTarget(TestDS);
+
+		testTestDSService.switchDS().then(res => {
+			console.log(res[0]?.caseName, res[1]?.caseName);
+		});
+	});
+});
