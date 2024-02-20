f657abe85b2b9d84889c4db917067b56f1863140
zhongjx <1433589043@qq.com>
2024-02-20 14:43:04 +0800

    修改读取不到日志依赖的错误打印

--- fastcar-core/src/annotation/stereotype/Log.ts
+++ fastcar-core/src/annotation/stereotype/Log.ts
@@ -10,7 +10,7 @@ export default function Log(category?: string) {
 		Reflect.defineProperty(target, propertyKey, {
 			get: (): Logger => {
 				let app: ApplicationInterface = Reflect.get(global, CommonConstant.FastcarApp);
-				let appid = app.getSetting(CommonConstant.APPId) || ""; //进行差异化区分
+				let appid = app?.getSetting(CommonConstant.APPId) || ""; //进行差异化区分
 				return app ? app.getLogger(appid ? `${appid}.${m}` : m) : console;
 			},
 		});
