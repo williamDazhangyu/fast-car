023acf730f8f12a615ab0635d47f9cf6fa2fb3fc
zhongjx <1433589043@qq.com>
2024-02-26 17:10:16 +0800

    修正错误

--- fastcar-core/src/annotation/scan/ComponentScanMust.ts
+++ fastcar-core/src/annotation/scan/ComponentScanMust.ts
@@ -1,22 +1,26 @@
+import "reflect-metadata";
 import * as path from "path";
 import * as fs from "fs";
 import { FastCarMetaData } from "../../constant/FastCarMetaData";
 
-export default function ComponentScanMust(target: any, ...names: string[]) {
-	let ScanPathList = FastCarMetaData.ComponentScanMust;
-	let list: string[] = Reflect.get(target.prototype, ScanPathList) || [];
+//和本包的相对路径
+export default function ComponentScanMust(...names: string[]) {
+	return function (target: any) {
+		let scanMust = FastCarMetaData.ComponentScanMust;
+		let list: string[] = Reflect.get(target.prototype, scanMust) || [];
 
-	for (let name of names) {
-		//可支持绝对路径
-		let p = path.join(require.main?.path || process.cwd() || "", name);
-		if (fs.existsSync(name)) {
-			p = name;
+		for (let name of names) {
+			//可支持绝对路径
+			let p = path.join(require.main?.path || process.cwd() || "", name);
+			if (fs.existsSync(name)) {
+				p = name;
+			}
+
+			if (!list.includes(p)) {
+				list.push(p);
+			}
 		}
 
-		if (!list.includes(p)) {
-			list.push(p);
-		}
-	}
-
-	Reflect.set(target.prototype, ScanPathList, list);
+		Reflect.set(target.prototype, scanMust, list);
+	};
 }
